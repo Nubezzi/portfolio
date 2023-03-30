6cfe23f46e88b1e8f4717fd7ea87a16b9c315362
ext-paavo.nurminen <ext-paavo.nurminen@accountorhr.fi>
2023-03-30 12:25:06 +0300

    added a gif background with cool effects and adjusted some layout factors

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,9 +3,24 @@
     let name = "Paavo Nurminen";
     let title = "Opiskelija & Kehittäjä";
     let buttonText = "Chekkaa lisää!";
+    let waiting = 0
+    function handleLoad() {
+      const hero = document.getElementById('hero');
+      hero.classList.add('loaded');
+    }
+    
+    const onload = el => {
+        waiting++
+        el.addEventListener('load', () => {
+          waiting--
+          if (waiting === 0) {
+            handleLoad()
+          }
+        })
+    }
   </script>
   
-  <section id="hero" class="hero">
+  <section id="hero" class="hero" on:load|once={handleLoad()}>
     <div class="container">
       <h1>{name}</h1>
       <h2>{title}</h2>
